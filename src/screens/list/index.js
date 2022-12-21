5212da3d2eb768cd92bad79d2a0d6d72c76b83d0
unknown <g.s.d.c@hotmail.com>
2022-12-21 16:28:31 -0300

    iniciando pagina de listas

--- src/screens/list/index.js
+++ src/screens/list/index.js
@@ -1,9 +1,49 @@
-import {React, Fragment} from "react";
+import { React, Fragment } from "react";
+import { Container, Section, Column, Title, Table } from "rbx"
+import Header from "../../components/header";
 
 const ListScreen = () => (
 
     <Fragment>
-        Hellow World!!!
+        <Header />
+        <Section size="medium" className="home">
+            <Container >
+                <Column.Group>
+                    <Column >
+                        <Table bordered fullwidth hoverable narrow striped>
+                            <Table.Head>
+                                <Table.Row>
+                                    <Table.Heading>One</Table.Heading>
+                                    <Table.Heading>Two</Table.Heading>
+                                    <Table.Heading>Three</Table.Heading>
+                                    <Table.Heading>four</Table.Heading>
+                                    <Table.Heading>five</Table.Heading>
+                                    
+                                </Table.Row>
+                            </Table.Head>
+                            <Table.Body>
+                                {[
+                                    ['Three', 'Four','Three', 'Four','Three'],
+                                    ['Five', 'Six','Five', 'Six','Three'],
+                                    ['Seven', 'Eight','Seven', 'Eight','Three'],
+                                    ['Nine', 'Ten','Nine', 'Ten','Three'],
+                                    ['Eleven', 'Twelve','Eleven', 'Twelve','Three'],
+                                ].map(([v1, v2,v3,v4,v5], i) => (
+                                    <Table.Row key={i}>
+                                        <Table.Cell>{v1}</Table.Cell>
+                                        <Table.Cell>{v2}</Table.Cell>
+                                        <Table.Cell>{v3}</Table.Cell>
+                                        <Table.Cell>{v4}</Table.Cell>
+                                        <Table.Cell>{v5}</Table.Cell>
+                                        
+                                    </Table.Row>
+                                ))}
+                            </Table.Body>
+                        </Table>
+                    </Column>
+                </Column.Group>
+            </Container>
+        </Section>
     </Fragment>
 )
 
